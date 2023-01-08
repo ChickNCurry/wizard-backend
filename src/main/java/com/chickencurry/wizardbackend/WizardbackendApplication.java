382fd72f60689edf0c6f8989b85e263b94cc982c
Maximilian Burzer <maxi.burzer@gmail.com>
2023-01-08 13:16:27 +0100

    initialize project structure

---
+++ src/main/java/com/chickencurry/wizardbackend/WizardbackendApplication.java
@@ -0,0 +1,13 @@
+package com.chickencurry.wizardbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WizardbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WizardbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chickencurry/wizardbackend/WizardbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chickencurry.wizardbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WizardbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
